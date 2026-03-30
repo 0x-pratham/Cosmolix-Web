7f2e7982391f1dded39ce0812af9db91b0759308
Vaibhav Bodade <vdbodade140705@gmail.com>
2026-03-30 12:21:53 +0530

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
+import CTA from './components/Cta'
+import Products from './components/Products'
+import Services from './components/Services'
 
 function App() {
   return (
     <div style={{ backgroundColor: '#FAFAF8', minHeight: '100vh' }}>
       <Navbar />
       <Hero />
+      <Services />
+      <Products />
+      <CTA />
+      <Footer />
     </div>
   )
 }
